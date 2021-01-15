6cff581117ecb8eff21bed543e74f486e2c3bee5
yuvraj umale <yuvrajumale07@gmail.com>
2021-01-15 21:57:09 +0530

    added Environment Vars

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-const secret = "thisisasecret"
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
