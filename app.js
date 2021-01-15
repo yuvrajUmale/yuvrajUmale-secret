e047186c48bf10b43887ecb747095d4e43364b2a
yuvraj umale <yuvrajumale07@gmail.com>
2021-01-15 21:48:12 +0530

    added dotenv files

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser');
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption');
+
+const app = express()
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded(
+    {
+        extended: true
+    }
+));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology:true});
+
+const userSchema = new mongoose.Schema({
+    email:String, 
+    password:String
+});
+
+const secret = "thisisasecret"
+userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", (req, res)=>{
+    res.render("home");
+});
+
+app.get("/login", (req, res)=>{
+    res.render("login");
+});
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const userPassword = req.body.password;
+
+    User.findOne({email : username}, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+            if(foundUser.password === userPassword){
+                res.render("secrets")
+            }
+            }
+        }
+    });
+});
+
+
+//register route
+app.get("/register", (req, res)=>{
+    res.render("register");
+});
+//post req for register route
+app.post("/register", (req, res)=>{
+   
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log("server is started on port 3000");
+})  
